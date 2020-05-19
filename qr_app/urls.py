e1cce9688941a7eca3697abc6274c13f3b14303b
suna <jisuna314@gmail.com>
2020-05-19 21:08:01 +0900

    사용자 별 로그인 페이지

--- qr_app/urls.py
+++ qr_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-    
+    path('visitorLogin/', views.visitorLogin, name = "visitorLogin"),
+    path('householdLogin/', views.householdLogin, name = "householdLogin"),
 ]
--- qr_app/views.py
+++ qr_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 
 # Create your views here.
+def visitorLogin(request):
+    return render(request, 'qr_app/visitorLogin.html')
+
+def householdLogin(request):
+    return render(request, 'qr_app/householdLogin.html')
