3ae2fc4b25d1fa7736ac2ff1d31e9936b7911774
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-05-21 21:25:07 +0900

    mysql database setting

--- QrSecurityProject/settings.py
+++ QrSecurityProject/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'QrSecurityProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstone',
+        'USER': 'root',
+        'PASSWORD': '19971001',
+        'HOST': 'ggiri.ctu4kidetxx4.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
