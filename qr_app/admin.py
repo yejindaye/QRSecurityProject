31460931a738b4f38bc6805d869397ae741c20f8
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-05-25 15:52:39 +0900

    2020.05.25
    database modeling & created admin superuser

--- qr_app/admin.py
+++ qr_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Resident)
+admin.site.register(Visitor)
+admin.site.register(Apartment)
+# admin.site.register(Building)
+# admin.site.register(Floor)
+# admin.site.register(Room)
--- qr_app/models.py
+++ qr_app/models.py
@@ -1,3 +1,62 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Resident(models.Model):
+    objects = models.Manager()
+    idx = models.BigAutoField(primary_key=True)
+    uid = models.TextField(blank=False, null=False)
+    pw = models.TextField(blank=False, null=False)
+    name = models.CharField(max_length=10, blank=True, null=True)
+    birth_year = models.IntegerField()
+
+class Device(models.Model):
+    MOBILE = 'mb'
+    TABLET = 'tl'
+    PC = 'pc'
+    TYPE_CHOICES = [
+        (MOBILE, '모바일'),
+        (TABLET, '테블릿'),
+        (PC, 'pc')
+    ]
+    objects = models.Manager()
+    idx = models.BigAutoField(primary_key=True)
+    device_type = models.CharField(
+        max_length=10, 
+        choices=TYPE_CHOICES,
+        default=MOBILE
+    )
+    os = models.CharField(max_length=10, blank=True)
+    version = models.CharField(max_length=10, blank=True)
+
+class Visitor(models.Model):
+    objects = models.Manager()
+    idx = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+    time = models.DurationField()
+    visited = models.BooleanField(default=False)
+
+class EntranceLog(models.Model):
+    objects = models.Manager()
+    idx = models.BigAutoField(primary_key=True)
+    date = models.DateTimeField(default=timezone.now)
+    visitor_idx = models.ForeignKey(Visitor, models.DO_NOTHING, db_column='visitor_idx')
+
+class Building(models.Model):
+    objects = models.Manager()
+    number = models.IntegerField()
+
+class Floor(models.Model):
+    objects = models.Manager()
+    number = models.IntegerField()
+
+class Room(models.Model):
+    objects = models.Manager()
+    number = models.IntegerField()
+
+class Apartment(models.Model):
+    objects = models.Manager()
+    building = models.ForeignKey(Building, on_delete = models.CASCADE, blank=True)
+    floor = models.ForeignKey(Floor, on_delete = models.CASCADE, blank=True)
+    room = models.ForeignKey(Room, on_delete = models.CASCADE, blank=True)
+
