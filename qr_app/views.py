d9f4011bf32f510ab50e23f65eaf4f3d83c539c6
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-06-27 00:54:24 +0900

    time error

--- qr_app/views.py
+++ qr_app/views.py
@@ -81,7 +81,6 @@ def resQrDisplay(request):
         #사용자 residentDB로 부터 hash값 받아오고 거기에 userid붙여서 qrCode에 저장
         hash = QrAppResident.objects.get(uid=request.session['r_id']).hash
         qrCode=user_id+hash
-    qrStartTime = time.time()
     return render(request, 'qr_app/resQrDisplay.html',{'qrCode':qrCode})  
 
 # 방문요청 허가 -> 방문요청 리스트 띄우기
